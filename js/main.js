599b01102137eda92a387a507f700d5b39a750c9
jonathan2011 <kaneo.2014@hotmail.com>
2024-05-05 22:54:15 -0500

    adiciona bucle y funciones

--- js/main.js
+++ js/main.js
@@ -1,34 +1,54 @@
-//let prompt("\n 1. " + "\n 2. "+ "\n 3. ");
-
 let fechaActual=2024;
 const fechaNacimiento= parseInt(prompt("introdusca su año de Nacimiento:"));
-let edad= fechaActual-fechaNacimiento;
-if( edad<18){
-    alert("Eres Menor de Edad, no puedes ingresar" +"\n Tienes "+edad+ "años")
 
+if(!isNaN(fechaNacimiento)){
+    if((fechaNacimiento.toString().length)!= 4 ){
+        alert("El numero ingresado de fecha no es correcto")
+    }
+    else{
+        let edad= fechaActual-fechaNacimiento;
+        if(edad<0){
+            alert("El numero ingresado de fecha no es correcto")     
+            }
+        else if(edad<18){
+            alert("Eres Menor de Edad, no puedes ingresar" +"\n Tienes "+edad+ "años")     
+            }
+        else{
+            alert ("Bienvenido a su desayuno en linea")
+            let producto=parseInt(prompt("Selecione uno de los siguientes desayunos que tenemos:"+
+            "\n 1. Huevos con pan y Chocolate" + "\n 2.Arepa con carne y cafe  "+ "\n 3.tamal con chocolate y pan "))   
+            
+            while((producto <=0)|| (producto >=4)){
+                alert("El usuario ingresó "+ producto+ " Opcion no Valida");
+                producto=parseInt(prompt("Selecione uno de los siguientes desayunos que tenemos:"+
+                "\n 1. Huevos con pan y Chocolate" + "\n 2.Arepa con carne y cafe  "+ "\n 3.tamal con chocolate y pan ")) 
+            }
+            function total(primerNumero, segundoNumero) {
+                return primerNumero +((primerNumero) *(segundoNumero));
+            }
+            switch (producto){
+                case 1:
+                    let valorDesayuno1= 10000;
+                    alert("Huevos con pan y Chocolate " +" le cuesta: $ "+valorDesayuno1);
+                    alert("total a pagar con iva es :"+ "$"+total(valorDesayuno1,0.19));
+                break; 
+                case 2:
+                    let valorDesayuno2= 20000;
+                    alert("Arepa con carne y cafe"  +" le cuesta: $ "+valorDesayuno2);
+                    alert("total a pagar con iva es :"+ "$"+total(valorDesayuno2,0.19));
+                break;
+                case 3:
+                    let valorDesayuno3= 30000;
+                    alert("tamal con chocolate y pan"  +" le cuesta: $ "+valorDesayuno3);
+                    alert("total a pagar con iva es :"+ "$"+total(valorDesayuno3,0.19));
+                break;
+                default:
+                    alert("Opcion no Valida")
+                break;   
+            }   
+        }
+    }
 }
 else{
-    alert ("Bienvenido a su desayuno en linea")
-
-    let producto=prompt("Selecione uno de los siguientes desayunos que tenemos:"+
-    "\n 1. Huevos con pan y Chocolate" + "\n 2.Arepa con carne y cafe  "+ "\n 3.tamal con chocolate y pan ")
-    
-        switch (producto){
-            case "1":
-                let valorDesayuno1= 10000;
-                alert("Huevos con pan y Chocolate " +" le cuesta: $ "+valorDesayuno1);
-            break;
-            case "2":
-                let valorDesayuno2= 20000;
-                alert("Arepa con carne y cafe"  +" le cuesta: $ "+valorDesayuno2);
-            break;
-            case "3":
-                let valorDesayuno3= 30000;
-                alert("tamal con chocolate y pan"  +" le cuesta: $ "+valorDesayuno3);
-            break;
-            default:
-                alert("Opcion no Valida")
-            break;   
-        }
-    
+alert("Opcion no Valida "+ fechaNacimiento+ " no es un año de Nacimiento " )
 }
